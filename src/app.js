501b2d914b12a44e04161e484ec9ded63586a302
guilherme <gui.mauricio@hotmail.com.br>
2020-08-19 22:54:03 -0300

    desafio1

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+ const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,65 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
+
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+ const {title, url, techs} = request.body;
+ 
+ const reposit = {
+   title,
+   url,
+   id: uuid(),
+   techs,
+   likes : 0,
+   }
+   repositories.push(reposit)
+ return response.json(reposit);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } =request.params;
+
+
+  const {title , url, techs} = request.body;
+  const repositIndex = repositories.findIndex(reposit => reposit.id === id);
+  if(repositIndex < 0){
+    return response.status(400).json({error: 'erro'})
+  }
+  const reposit ={
+    title,
+    url,
+    techs,
+    id,
+
+  }
+
+  repositories [repositIndex] = reposit;
+  return response.json(reposit);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(reposit => reposit.id === id)
+if (repositoryIndex < 0) {
+  return response.status(400).json({error: 'erro'})
+}
+  repositories.splice(repositoryIndex, 1);
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+ const { id } = request.params;
+
+
+ 
+
+
+ const repository = repositories.find(repository => repository.id === id);
+  repository.likes +=1;
+  return response.json(repository);
 });
 
 module.exports = app;
